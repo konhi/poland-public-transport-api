a48b225d9aa5167991476b05e5779276a3c44c91
konhi <hello.konhi@gmail.com>
2022-01-28 19:44:34 +0100

    add firebase

--- src/app.ts
+++ src/app.ts
@@ -23,4 +23,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
  */
 app.get("/", homeController.index);
 
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
 export default app;
