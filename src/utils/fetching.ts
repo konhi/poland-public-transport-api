0987ae9dee5ab203b4bc703c10a4db38c4b65b23
konhi <hello.konhi@gmail.com>
2022-02-14 17:16:43 +0100

    feat: :sparkles: useragent for fetch

--- src/utils/fetching.ts
+++ src/utils/fetching.ts
@@ -3,7 +3,13 @@ import logger from "./logger";
 
 export async function getJson(url: string): Promise<any> {
   try {
-    const response = await fetch(url);
+    const userAgent = 'Non-commercial Poland Public Transport API; Contact at: https://github.com/konhi/poland-public-transport-api';
+
+    const response = await fetch(url, {
+      headers: {
+        'User-agent': userAgent
+      }
+    });
   
     return await response.json();
   } catch (error) {
