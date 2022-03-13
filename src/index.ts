36cb83c99012088c732da6333ef42ad9c3a8ba72
konhi <hello.konhi@gmail.com>
2022-03-13 13:54:00 +0100

    fix: :bug: handle errors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { Request } from 'itty-router'
 import { missing, ThrowableRouter } from 'itty-router-extras'
 import zielonagoraRouter from './handlers/zielonagora'
 
-const router = ThrowableRouter({ base: '/v1' })
+const router = ThrowableRouter()
 
 router
-  .all('/zielonagora/*', zielonagoraRouter.handle)
+  .all('/v1/zielonagora/*', zielonagoraRouter.handle)
   .all('/*', () => missing())
 
 
